594c754a5edb3ad03dfb797fe68a155cbf926d7b
yanoshi-1009 <y.idakko@gmail.com>
2022-12-21 15:32:41 +0900

    first

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-(() => {
+(async () => {
   "use strict";
 
   // URLチェック
-  if (!/^https:\/\/.+\.cybozu\.com/.test(location.href)) {
+  if (!/^https:\/\/.+\.cybozu\.com\/k/.test(location.href)) {
     window.alert("このサイトでは実行できません。");
     return;
   }
 
-  const hoge;
+  // Papaparseの読み込み
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js";
+  document.body.appendChild(script);
+
+  try {
+    const users = await kintone
+      .api(kintone.api.url("/k/api/guest/list.json"), "POST", {})
+      .then((resp) => resp.result.items);
+    const csv = Papa.unparse(users);
+    const blob = new Blob([csv], { type: "text/csv" });
+
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = "guests.csv";
+    a.click();
+  } catch (e) {
+    window.alert(`ユーザー情報の取得に失敗しました。\n${e.message}`);
+  }
 })();
